38807ead8f32a3a2c325fc0f0b3053d078ae5d0a
robinweitzel <robin.weitzel.rw@gmail.com>
2025-04-25 08:52:58 +0200

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name][ext]'
+                }
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public', to: '' }
+            ]
+        })
+    ]
+};
